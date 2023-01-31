d105d474977455eda70dec4e0f34241da64aa99c
srikanthkatta07 <kaanth.katta@gmail.com>
2023-01-31 19:13:33 +0530

    Remove unused code from boilerplate

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,27 +1,13 @@
 const express = require('express');
-const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
-const docsRoute = require('./docs.route');
 const config = require('../../config/config');
 
 const router = express.Router();
 
 const defaultRoutes = [
   {
-    path: '/auth',
-    route: authRoute,
-  },
-  {
     path: '/users',
     route: userRoute,
-  },
-];
-
-const devRoutes = [
-  // routes available only in development mode
-  {
-    path: '/docs',
-    route: docsRoute,
   },
 ];
 
@@ -29,11 +15,6 @@ defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
 
-/* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+
 
 module.exports = router;
