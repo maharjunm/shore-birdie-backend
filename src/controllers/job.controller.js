5cfd5439fae31f2c9313016af47d759740ce06f0
amrathmd <amrathmd.mohammed@gmail.com>
2023-02-10 08:32:04 +0530

    commented code removed

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -2,12 +2,6 @@ const { jobService } =require('../services/index');
 const { Job }=require('../models/index');
 
 const CreateJob= async (req,res)=>{
- /* const job=new Job(req.body);
-  job.save().then(res=>{
-    console.log("successfully added");
-
-  }).catch(err=>{
-    console.log(err)});*/
   const job=await jobService.createJob(req.body);
   res.status(201).send(job);
 
