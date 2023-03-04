effdbe72e1d1febf7c75f4476a43070de78cab31
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-03-04 15:26:13 +0530

    some changes

--- src/routes/v1/payment.route.js
+++ src/routes/v1/payment.route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {Product} = require('../../models');
 const validate = require('../../middlewares/validate');
 const paymentValidation = require('../../validations/payment.validation');
 const paymentController = require('../../controllers/payment.controller');
@@ -7,6 +8,18 @@ const router = express.Router();
 
   router
     .route('/')
-    .post(validate(paymentValidation.createPayment),  paymentController.createPayment)
+    .post((req,res)=>{
+      console.log("body" ,req.body);
+      const {token ,product} = req.body;
+      Product.create(product);
+      res.json({
+        "title":"stripe payment info",
+        "status":"success",
+        "information":{
+          "token":token,
+          "product":product
+        }
+      });
+    })
     
 module.exports = router;
--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const { Payment } = require('../models');
+const { Payment,Product } = require('../models');
 const ApiError = require('../utils/ApiError');
 const uuid = require("uuid").v4
 const bodyparser = require('body-parser')
@@ -8,14 +8,18 @@ const stripe = require("stripe")(config.stripekey)
 
 const createPayment = async (paymentBody) => {
   const { token, product } = paymentBody;
-  if (await Payment.isProductTaken(token.email)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Email  has already purchased the product');
-  }
-  const customer = await stripe.customers.create({
-        email: token.email,
-        source:token.id
-  });
-  
+  Payment.create(product);
+  console.log("product:" ,product);
+  // if (await Payment.isProductTaken(token.email)) {
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Email  has already purchased the product');
+  // }
+  Product.create(product);
+  console.log("token",token);
+  // const customer = await stripe.customers.create({
+  //       email: token.email,
+  //       source:token.id
+  // }).catch((err) => { console.error(' STRIPE ERROR: ', error); })
+  // console.log("customer",customer);
   return {
     "title":"stripe payment info",
     "status":"success",
